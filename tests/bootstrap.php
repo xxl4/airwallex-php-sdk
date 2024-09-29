cb1c4dea8fe01171b41f9af204b6fd3454c0679c
Steve <34465153+xxl4@users.noreply.github.com>
2024-09-29 14:25:59 +0800

    update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,32 @@
 <?php
-declare(strict_types=1);
 
-include_once __DIR__ . '/../.env.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
 
-if (!isset($variables)) {
-    throw new Exception('Environment variables could not been found');
-}
+require __DIR__.'/../vendor/autoload.php';
 
-foreach ($variables as $key => $value) {
-    putenv("$key=$value");
-    echo $key."--".$value."\r\n";
-}
+use Carbon\Carbon;
+
+/*
+|--------------------------------------------------------------------------
+| Set The Default Timezone
+|--------------------------------------------------------------------------
+|
+| Here we will set the default timezone for PHP. PHP is notoriously mean
+| if the timezone is not explicitly set. This will be used by each of
+| the PHP date and date-time functions throughout the application.
+|
+*/
+
+date_default_timezone_set('UTC');
+
+Carbon::setTestNow(Carbon::now());
